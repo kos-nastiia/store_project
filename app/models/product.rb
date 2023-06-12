c573528cdbafd23c7d6d572d61b1f074990476a0
kos-nastiia <kostyrka.anastasiia@gmail.com>
2023-06-12 08:40:06 +0300

    add controllers and views on Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
   has_many :orders, through: :product_orders
 
   validates :title, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :balance, numericality:  { greater_than_or_equal_to: 0 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :products
 end
